3a2773c1a209b81cd55c30acd7ebb6330004cc8f
Elman <200522elmanok>
2024-09-20 19:00:19 +0500

    fix: header font-size

--- src/components/toolBar/ToolBar.tsx
+++ src/components/toolBar/ToolBar.tsx
@@ -2,35 +2,35 @@ import { NavLink } from "react-router-dom";
 
 const ToolBar = () => {
   return (
-    <header className="w-[80%] mx-auto py-5 flex justify-between items-center">
-      <NavLink to={"/"}>
-        <p className="text-3xl font-semibold">WEBIX</p>
+    <header className="w-[90%] mx-auto py-5 flex justify-between items-center border-b border-gray-300 ">
+      <NavLink to={"/"} className="hover:text-gray-500 transition-colors duration-300">
+        <p className="text-2xl desktop:text-3xl desktop2:text-4xl font-semibold">WEBIX</p>
       </NavLink>
-      <nav className="text-[20px] flex justify-between w-[60%] hover:text-gray-500">
-        <NavLink to={"/"}>
+      <nav className="desktop:text-lg desktop2:text-2xl flex justify-between w-[60%]">
+        <NavLink to={"/"} className="hover:text-gray-500 transition-colors duration-300">
           <p>Финансы</p>
         </NavLink>
-        <NavLink to={"/"}>
+        <NavLink to={"/"} className="hover:text-gray-500 transition-colors duration-300">
           <p>Утилиты</p>
         </NavLink>
-        <NavLink to={"/"}>
+        <NavLink to={"/"} className="hover:text-gray-500 transition-colors duration-300">
           <p>Здоровье</p>
         </NavLink>
-        <NavLink to={"/"}>
+        <NavLink to={"/"} className="hover:text-gray-500 transition-colors duration-300">
           <p>Игры</p>
         </NavLink>
-        <NavLink to={"/"}>
+        <NavLink to={"/"} className="hover:text-gray-500 transition-colors duration-300">
           <p>Социальные сети</p>
         </NavLink>
-        <NavLink to={"/"}>
+        <NavLink to={"/"} className="hover:text-gray-500 transition-colors duration-300">
           <p>Путешествия</p>
         </NavLink>
       </nav>
-      <div className="w-[19%] flex justify-between items-center text-[20px]">
-        <NavLink to={"/"}>
+      <div className="flex items-center desktop:text-lg desktop2:text-2xl">
+        <NavLink to={"/"} className="hover:text-gray-500 transition-colors duration-300">
           <p>О нас</p>
         </NavLink>
-        <div className="bg-gray-100 p-3 text-[17px]">
+        <div className="bg-gray-100 p-3 ml-5 desktop:text-base desktop2:text-lg">
           <p>7 (701) 234 56 78</p>
         </div>
       </div>
