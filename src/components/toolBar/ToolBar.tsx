b1fad355a8f54b31541a618a3cda8855a4533b25
Elman <200522elmanok>
2024-09-20 18:03:31 +0500

    create: desktop header

--- src/components/toolBar/ToolBar.tsx
+++ src/components/toolBar/ToolBar.tsx
@@ -1,7 +1,41 @@
+import { NavLink } from "react-router-dom";
+
 const ToolBar = () => {
-  return <header className="w-[80%] mx-auto py-5">
-    <p className="text-3xl font-bold">WEBIX</p>
-  </header>
-}
+  return (
+    <header className="w-[80%] mx-auto py-5 flex justify-between items-center">
+      <NavLink to={"/"}>
+        <p className="text-3xl font-semibold">WEBIX</p>
+      </NavLink>
+      <nav className="text-[20px] flex justify-between w-[60%] hover:text-gray-500">
+        <NavLink to={"/"}>
+          <p>Финансы</p>
+        </NavLink>
+        <NavLink to={"/"}>
+          <p>Утилиты</p>
+        </NavLink>
+        <NavLink to={"/"}>
+          <p>Здоровье</p>
+        </NavLink>
+        <NavLink to={"/"}>
+          <p>Игры</p>
+        </NavLink>
+        <NavLink to={"/"}>
+          <p>Социальные сети</p>
+        </NavLink>
+        <NavLink to={"/"}>
+          <p>Путешествия</p>
+        </NavLink>
+      </nav>
+      <div className="w-[19%] flex justify-between items-center text-[20px]">
+        <NavLink to={"/"}>
+          <p>О нас</p>
+        </NavLink>
+        <div className="bg-gray-100 p-3 text-[17px]">
+          <p>7 (701) 234 56 78</p>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default ToolBar;
