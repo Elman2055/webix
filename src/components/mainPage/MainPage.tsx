01bb5d51fdcdaf3ccbad19fe0feb1787c85c2ada
Elman <200522elmanok>
2024-09-21 22:42:32 +0500

    create: main info block

--- src/components/mainPage/MainPage.tsx
+++ src/components/mainPage/MainPage.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { TMain, TProduct } from "../../types/types.data";
+import mainBg from "../../../public/mainBg.png";
 
 const MainPage = ({
   mainProduct,
@@ -36,11 +36,11 @@ const MainPage = ({
         <p className="text-3xl font-medium">Шаблоны для здоровья и медицины</p>
         <button className="bg-gray-100 p-2">Смотреть все</button>
       </div>
-      <div className="grid grid-cols-4 gap-5">
+      <div className="grid grid-cols-4 gap-5 border-b border-gray-300 pb-4">
         {firstRowProducts.map((el: TProduct) => (
           <div key={el.product_id} className="flex flex-col justify-between">
             <img
-            className="cursor-pointer"
+              className="cursor-pointer object-cover"
               src={`https://logohub.kz/api/products/previewImage/${el.image_preview}`}
               alt="product"
             />
@@ -52,6 +52,24 @@ const MainPage = ({
             <button className="w-full py-2 bg-gray-100 hover:bg-green-500 hover:text-white transition-colors duration-300 mt-3">Купить</button>
           </div>
         ))}
+      </div>
+      <div className="bg-gray-100 p-7 mt-10">
+        <p className="text-4xl font-medium">Знакомьтесь с нами</p>
+        <img src={mainBg} alt="info background" className="my-5"/>
+        <div className="flex gap-3">
+          <p>
+            Мы — ведущий цифровой магазин, специализирующийся на продаже готовых
+            сайтов. Наша миссия — предоставить вам качественные, профессионально
+            разработанные решения, которые помогут вашему бизнесу выделиться
+            онлайн.
+          </p>
+          <p>
+            Мы предлагаем широкий выбор шаблонов для различных ниш и целей, от
+            корпоративных сайтов до интернет-магазинов. Наша команда постоянно
+            обновляет и улучшает наши предложения, чтобы соответствовать самым
+            современным требованиям и трендам.
+          </p>
+        </div>
       </div>
     </div>
   );
