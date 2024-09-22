c5a72ccccdd72c4e49c7c64498b56ef10bb07a3b
Elman <200522elmanok>
2024-09-23 04:41:41 +0500

    create: filter categories

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,18 +1,25 @@
-import React from "react";
-
 const Categories = () => {
   return (
     <div className="w-[90%] mx-auto">
-      <div>
+      <div className="flex gap-5">
         <div className="w-[40%] bg-gray-100 p-5">
           <div className="flex items-center justify-between pb-5">
-            <p className="text-3xl font-medium">Утилиты</p>
-            <p>132 товара</p>
+            <p className="text-4xl desktop2:text-5xl font-medium">Утилиты</p>
+            <p className="desktop2:text-2xl">132 товара</p>
           </div>
-          <p>
+          <p className="desktop2:text-2xl">
             Шаблоны для утилитарных сайтов обеспечивают идеальные решения для
             создания функциональных и эффективных онлайн-ресурсов
           </p>
+        </div>
+        <div className="bg-gray-300 w-px"></div>
+        <div className="flex flex-col justify-between">
+          <p className="text-xl">Выбрать по:</p>
+          <div className="w-[500px] flex flex-wrap gap-3">
+            {["По популярности", "По возрастанию цены", 'По убывании цены', 'Новинки', 'Скидки'].map((el) => (
+              <button key={el} className="bg-gray-100 p-3 hover:bg-green-500 hover:text-white transition-colors duration-300">{el}</button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
