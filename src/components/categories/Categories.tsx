7a0b8f26e35aa9e70b463f9061dc8928247be5b0
Elman <200522elmanok>
2024-09-23 05:21:34 +0500

    create: mobile categories page

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -3,11 +3,13 @@ import ProductsCards from "../productsCards/ProductsCards";
 
 const Categories = ({ title, quantity, products }: TCategories) => {
   return (
-    <div className="w-[90%] mx-auto">
-      <div className="flex gap-5">
-        <div className="w-[40%] bg-gray-100 p-5">
+    <div className="w-[500px] max-w-[90%] laptop:w-[90%] mx-auto">
+      <div className="flex flex-col laptop:flex-row gap-5">
+        <div className="w-full laptop:w-[45%] bg-gray-100 p-5">
           <div className="flex items-center justify-between pb-5">
-            <p className="text-4xl desktop2:text-5xl font-medium">{title}</p>
+            <p className="text-2xl laptop:text-4xl desktop2:text-5xl font-medium mr-2">
+              {title}
+            </p>
             <p className="desktop2:text-2xl">{quantity} товара</p>
           </div>
           <p className="desktop2:text-2xl">
@@ -17,15 +19,22 @@ const Categories = ({ title, quantity, products }: TCategories) => {
         </div>
         <div className="bg-gray-300 w-px"></div>
         <div className="flex flex-col justify-between">
-            <p className="text-xl desktop2:text-3xl desktop2:font-medium">Выбрать по:</p>
-            <div className="w-[500px] flex flex-wrap gap-3">
-              {["По популярности", "По возрастанию цены", 'По убывании цены', 'Новинки', 'Скидки'].map((el) => (
-                <button key={el} className="bg-gray-100 p-3 hover:bg-green-500 hover:text-white transition-colors duration-300 desktop2:text-2xl">{el}</button>
-              ))}
-            </div>
+          <p className="text-xl font-bold desktop2:text-3xl laptop:font-medium mb-3">
+            Выбрать по:
+          </p>
+          <div className="w-[500px] max-w-full flex flex-wrap gap-3">
+            {["По популярности", "По возрастанию цены", "По убывании цены", "Новинки", "Скидки",].map((el) => (
+              <button
+                key={el}
+                className="bg-gray-100 p-3 text-[15px] laptop:text-lg hover:bg-green-500 hover:text-white transition-colors duration-300 desktop2:text-2xl"
+              >
+                {el}
+              </button>
+            ))}
           </div>
+        </div>
       </div>
-      <ProductsCards title="" rowProducts={products}/>
+      <ProductsCards title="" rowProducts={products} />
     </div>
   );
 };
